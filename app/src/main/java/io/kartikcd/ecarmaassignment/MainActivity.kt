c60c9da81942272d10175f1e406788d4013df812
KartikCD <chawdakartik@gmail.com>
2021-09-23 02:11:13 +0530

    asset list adapter tested and implemented

--- app/src/main/java/io/kartikcd/ecarmaassignment/MainActivity.kt
+++ app/src/main/java/io/kartikcd/ecarmaassignment/MainActivity.kt
@@ -4,34 +4,43 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import io.kartikcd.ecarmaassignment.adapter.AssetListAdapter
 import io.kartikcd.ecarmaassignment.adapter.CommunityListAdapter
 import io.kartikcd.ecarmaassignment.models.Community
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_register)
+        setContentView(R.layout.fragment_community_details)
 
-//        val recyclerview = findViewById(R.id.recyclerview_community) as RecyclerView
-//
-//        val communities = listOf(
-//            Community("Brigade Sympony", true),
-//            Community("Community 2", false),
-//            Community("Community 3", false),
-//            Community("Community 4", false),
-//            Community("Community 5", false)
-//        )
-//
-//        val communityListAdapter = CommunityListAdapter()
-//
-//        recyclerview.layoutManager = LinearLayoutManager(applicationContext)
-//        recyclerview.adapter = communityListAdapter
-//
-//        communityListAdapter.submitList(communities)
-//
-//        communityListAdapter.setOnCommunityClickListener {
-//
-//        }
+        val recyclerview = findViewById<RecyclerView>(R.id.recyclerview_assets)
+
+        val communities = listOf(
+            Community("Brigade Sympony", true),
+            Community("Community 2", false),
+            Community("Community 3", false),
+            Community("Community 4", false),
+            Community("Community 5", false)
+        )
+
+        println("Debug: ${recyclerview.toString()}")
+
+        val assets = listOf(
+            "Association",
+            "Block details",
+            "Manpower",
+            "Assets",
+            "Amenities",
+            "AMC",
+            "Documents"
+        )
+
+        val communityListAdapter = AssetListAdapter()
+
+        recyclerview.layoutManager = LinearLayoutManager(applicationContext)
+        recyclerview.adapter = communityListAdapter
+
+        communityListAdapter.submitList(assets)
 
     }
 }
--- app/src/main/java/io/kartikcd/ecarmaassignment/adapter/AssetListAdapter.kt
+++ app/src/main/java/io/kartikcd/ecarmaassignment/adapter/AssetListAdapter.kt
@@ -1,4 +1,51 @@
 package io.kartikcd.ecarmaassignment.adapter
 
-class AssetListAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import io.kartikcd.ecarmaassignment.databinding.ListAssetBinding
+import io.kartikcd.ecarmaassignment.models.Community
+
+class AssetListAdapter: ListAdapter<String, AssetListAdapter.AssetListViewHolder>(AssetDiffUtilCallback) {
+
+    private var onAssetClickListener: ((String) -> Unit) ?= null
+
+    inner class AssetListViewHolder(val binding: ListAssetBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(name: String) {
+            binding.apply {
+                textCompanyName.text = name
+                root.setOnClickListener {
+
+                }
+            }
+        }
+    }
+
+    fun setOnAssetClickListener(listener: (String) -> Unit) {
+        onAssetClickListener = listener
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AssetListViewHolder {
+        val binding = ListAssetBinding
+            .inflate(LayoutInflater.from(parent.context), parent, false)
+        return AssetListViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: AssetListViewHolder, position: Int) {
+        getItem(position)?.let {
+            holder.bind(it)
+        }
+    }
+}
+
+object AssetDiffUtilCallback: DiffUtil.ItemCallback<String>() {
+    override fun areItemsTheSame(oldItem: String, newItem: String): Boolean {
+        return oldItem == newItem
+    }
+
+    override fun areContentsTheSame(oldItem: String, newItem: String): Boolean {
+        return oldItem == newItem
+    }
 }
