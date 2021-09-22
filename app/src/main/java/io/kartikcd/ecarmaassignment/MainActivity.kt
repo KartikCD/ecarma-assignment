c34e176dc69d30857d64d209f34480b2a5853dd0
KartikCD <chawdakartik@gmail.com>
2021-09-23 03:52:06 +0530

    add block detail screen added

--- app/src/main/java/io/kartikcd/ecarmaassignment/MainActivity.kt
+++ app/src/main/java/io/kartikcd/ecarmaassignment/MainActivity.kt
@@ -13,41 +13,41 @@ import io.kartikcd.ecarmaassignment.models.Community
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_association_detail)
+        setContentView(R.layout.fragment_add_block)
 
-        val recyclerview = findViewById<RecyclerView>(R.id.recyclerview_associations)
-
-        val communities = listOf(
-            Community("Brigade Sympony", true),
-            Community("Community 2", false),
-            Community("Community 3", false),
-            Community("Community 4", false),
-            Community("Community 5", false)
-        )
-
-        val associations = listOf(
-            Association("President", "Rahul Raj", "Flat 314", "9987746997", "xyz@gmail.com"),
-            Association("Vice President", "Ankit Gupta", "Flat 318", "8116162777", "xyz@gmail.com")
-        )
-
-        println("Debug: ${recyclerview.toString()}")
-
-        val assets = listOf(
-            "Association",
-            "Block details",
-            "Manpower",
-            "Assets",
-            "Amenities",
-            "AMC",
-            "Documents"
-        )
-
-        val communityListAdapter = AssociationListAdapter()
-
-        recyclerview.layoutManager = LinearLayoutManager(applicationContext)
-        recyclerview.adapter = communityListAdapter
-
-        communityListAdapter.submitList(associations)
+//        val recyclerview = findViewById<RecyclerView>(R.id.recyclerview_associations)
+//
+//        val communities = listOf(
+//            Community("Brigade Sympony", true),
+//            Community("Community 2", false),
+//            Community("Community 3", false),
+//            Community("Community 4", false),
+//            Community("Community 5", false)
+//        )
+//
+//        val associations = listOf(
+//            Association("President", "Rahul Raj", "Flat 314", "9987746997", "xyz@gmail.com"),
+//            Association("Vice President", "Ankit Gupta", "Flat 318", "8116162777", "xyz@gmail.com")
+//        )
+//
+//        println("Debug: ${recyclerview.toString()}")
+//
+//        val assets = listOf(
+//            "Association",
+//            "Block details",
+//            "Manpower",
+//            "Assets",
+//            "Amenities",
+//            "AMC",
+//            "Documents"
+//        )
+//
+//        val communityListAdapter = AssociationListAdapter()
+//
+//        recyclerview.layoutManager = LinearLayoutManager(applicationContext)
+//        recyclerview.adapter = communityListAdapter
+//
+//        communityListAdapter.submitList(associations)
 
     }
 }
